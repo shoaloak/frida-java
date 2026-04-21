10c52d4a3bd14d2d77f333f8f4ca784b225517ad
shoaloak <dev@axelkoolhaas.nl>
2026-04-21 13:58:03 +0200

    Add ScriptOptions class and related enums - Create ScriptOptions for script creation configuration - Create ScriptRuntime enum (DEFAULT, QJS, V8) - Create SnapshotTransport enum (INLINE, SHARED_MEMORY) - Support name, snapshot, snapshot transport, and runtime settings - Implements AutoCloseable for proper resource cleanup - Completes todo 19

---
+++ frida-java-core/src/main/java/nl/axelkoolhaas/frida_java/frida/ScriptRuntime.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2026 Axel Koolhaas
+ *
+ * This file is part of frida-java.
+ *
+ * frida-java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * frida-java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with frida-java.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package nl.axelkoolhaas.frida_java.frida;
+
+/**
+ * Script runtime engine selection
+ */
+public enum ScriptRuntime {
+    DEFAULT(0),
+    QJS(1),
+    V8(2);
+
+    private final int value;
+
+    ScriptRuntime(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static ScriptRuntime fromValue(int value) {
+        for (ScriptRuntime runtime : values()) {
+            if (runtime.value == value) {
+                return runtime;
+            }
+        }
+        throw new IllegalArgumentException("Unknown ScriptRuntime value: " + value);
+    }
+}
+
